ba9812cbed11266c0ad4da4e3c84af71185fcedf
MushfiqMoon <mushfiq.moon013@google.com>
2023-01-01 02:45:15 +0600

    initial setup with some get api and data

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const chats = require('./data/data')
+
+const app = express()
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api/chats', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find(c => c._id === req.params.id)
+    res.send(singleChat)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
